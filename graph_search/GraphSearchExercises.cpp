467f364c14e05028ec8e016094eb4af6b2f0368f
Tanzim Mokammel <tanzim.mokammel@moonfare.com>
2020-06-10 18:08:21 +0200

    bfs

--- graph_search/GraphSearchExercises.cpp
+++ graph_search/GraphSearchExercises.cpp
@@ -339,7 +339,7 @@ std::list<IntPair> graphBFS(const IntPair &start, const IntPair &goal, const Gri
     // TODO: Your code here!
     // We'll need to loop over the neighbors that are the points adjacent to curPoint.
     // Get a copy of the set of neighbors we're going to loop over.
-    GridGraph::NeighborSet neighbors; // Change this...
+    GridGraph::NeighborSet neighbors = graph.adjacencyMap.at(curPoint); // Change this...
     // =====================================================================
 
     for (auto neighbor : neighbors)
@@ -348,7 +348,7 @@ std::list<IntPair> graphBFS(const IntPair &start, const IntPair &goal, const Gri
       // ==================================================================
       // TODO: Your code here!
       // Check whether the neighbor has already been visited.
-      bool neighborWasAlreadyVisited = false; // Change this...
+      bool neighborWasAlreadyVisited = visitedSet.count(neighbor); // Change this...
       // ==================================================================
 
       // If this adjacent vertex has NOT been visited before, we will visit it now.
@@ -364,13 +364,15 @@ std::list<IntPair> graphBFS(const IntPair &start, const IntPair &goal, const Gri
         // since curPoint has just led to the discovery of this neighbor for
         // the first time.
         // ...
+        pred[neighbor] = curPoint;
 
         // Add neighbor to the visited set.
         // ...
+        visitedSet.insert(neighbor);
 
         // Push neighbor into the exploration queue.
         // ...
-
+        exploreQ.push(neighbor);
         // ================================================================
 
         // Check if we've taken too many steps so far.
